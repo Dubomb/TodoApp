22d0ac03db05b6a341c599ac2ce067dc40db39df
Dubomb <dubomb1011@gmail.com>
2023-12-26 19:35:20 -0500

    Successfully connected to the MySQL database as root

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-var mysql = require('mysql'); 
+var mysql = require('mysql2'); 
+var fs = require('node:fs');
 
-console.log("Connecting!");
+console.log('Connecting...');
+
+const pass = fs.readFileSync('login.txt');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: pass,
+    database: 'todo'
+});
+
+connection.connect((error) => {
+    if (error) {
+        console.log('Connection failed with error: ' + error.message);
+        return;
+    }
+
+    console.log('Connection successful!');
+
+    connection.end();
+});
