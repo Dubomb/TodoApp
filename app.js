3c6071248ae136b432a950bed494a6080fdb74b7
Dubomb <dubomb1011@gmail.com>
2023-12-27 13:19:59 -0500

    Modified application to set up a new database with tables if it does not already exist

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var fs = require('node:fs');
 
 console.log('Connecting...');
 
-const pass = fs.readFileSync('login.txt');
+const pass = fs.readFileSync('login.txt', 'utf-8');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
+    user: 'pnapoli',
     password: pass,
     database: 'todo'
 });
@@ -18,7 +18,32 @@ connection.connect((error) => {
         return;
     }
 
-    console.log('Connection successful!');
+    connection.query('create database if not exists todo', (err) => {
+        if (err) {
+            console.log('Issue creating the database.');
+            return;
+        }
+    });
 
-    connection.end();
+    connection.query('use todo');
+
+    connection.query('show tables', (err, results) => {
+        if (results.length === 0) {
+            const sqlQueries = fs.readFileSync('setup.sql', 'utf-8').split(';');
+            sqlQueries.forEach((query) => {
+                if (query.trim() === '') {
+                    return;
+                }
+
+                connection.query(query.trim(), (err) => {
+                    if (err) {
+                        console.log('Issue creating database tables. ' + err.message);
+                        return;
+                    }
+                });
+            });
+        }
+    });
+
+    console.log('Connection successful!');
 });
