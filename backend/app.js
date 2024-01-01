13752594fdcec58a6be737f22621aacc8f65b2e1
Dubomb <dubomb1011@gmail.com>
2024-01-01 18:11:02 -0500

    Added GET, POST, PUT, and DELETE requests for task categories

--- backend/app.js
+++ backend/app.js
@@ -75,6 +75,66 @@ expressApp.delete('/api/tasks', (req, res) => {
     });
 });
 
+expressApp.get('api/categories', (req, res) => {
+    connection.query('select * from category', (err, results) => {
+        if (err) { 
+            console.log('Error: ' + err.message);
+            res.status(500).json({success: false, message: 'Failed to retrieve database information.'});
+            return;
+        }
+        
+        res.json({success: true, message: results});
+    });
+});
+
+expressApp.post('api/categories', (req, res) => {
+    const query = 'insert into category (category_ID, name) values (?, ?)';
+    const data = req.body;
+    const params = [data.category_ID, data.name];
+    
+    connection.query(query, params, (err) => {
+        if (err) {
+            console.log('Error: ' + err.message);
+            res.status(500).json({success: false, message: 'Failed to insert data into database.'});
+            return;
+        }
+
+        res.json({success: true, message: 'Successfully inserted data into database.'});
+    });
+});
+
+expressApp.put('api/categories', (req, res) => {
+    const query = 'update category set name = ? where category_ID = ?';
+    const data = req.body;
+    const params = [data.name, data.category_ID];
+
+    connection.query(query, params, (err) => {
+        if (err) {
+            console.log('Error: ' + err.message);
+            res.status(500).json({success: false, message: 'Failed to update data in database.'});
+            return;
+        }
+
+        res.json({success: true, message: 'Successfully updated data in database.'});
+    });
+});
+
+expressApp.delete('api/categories', (req, res) => {
+    const query = 'delete from task where category_ID = ?';
+    const data = req.body;
+    const params = [data.category_ID];
+
+    connection.query(query, params, (err) => {
+        if (err) {
+            console.log('Error: ' + err.message);
+            res.status(500).json({success: false, message: 'Failed to delete data from database.'});
+            return;
+        }
+
+        res.json({success: true, message: 'Successfully deleted data from database.'});
+    });
+});
+
 expressApp.listen(port, () => {
     console.log('Server running on ' + port);
 });
