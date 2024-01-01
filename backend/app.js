6405a94f02777172a10c6719075b8d609adb7a1a
Dubomb <dubomb1011@gmail.com>
2024-01-01 15:53:20 -0500

    Added GET requests for server status and getting tasks from the database

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,50 @@ const express = require('express');
 const expressApp = express();
 const port = 3001;
 
-expressApp.get('/', (req, res) => {
-    res.send('Hello!');
+const cors = require('cors');
+
+expressApp.use(cors());
+expressApp.use(express.json());
+
+expressApp.get('/status', (req, res) => {
+    res.json({message: 'The server is ready.'});
+});
+
+expressApp.get('/api/gettasks', (req, res) => {
+    connection.query('select * from task', (err, results) => {
+        if (err) { 
+            res.status(500).json({success: false, message: 'Failed to retrieve database information.'});
+            console.log('Failed to retrieve database information.');
+            return;
+        }
+        
+        res.json({success: true, message: results});
+    });
+});
+
+expressApp.get('/test', (req, res) => {
+    let r = 'what';
+
+    connection.query('select name from status', (err, results) => {
+        if (err) {
+            res.status(500).json({success: false, message: 'Database failed to fetch information'});
+            return;
+        }
+
+        res.json({message: results});
+    });
+});
+
+expressApp.post('/number', (req, res) => {
+    const query = 'insert into test (a) values (?)';
+    connection.query(query, [req.body.a], (err, results) => {
+        if (err) {
+            res.json({message: 'Error: ' + err.message});
+            return;
+        }
+
+        res.json({message: 'Successfully inserted data into database'});
+    })
 });
 
 expressApp.listen(port, () => {
@@ -59,6 +101,4 @@ connection.connect((error) => {
     });
 
     console.log('Connection successful!');
-
-    connection.end();
 });
