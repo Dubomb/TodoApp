674ce0b72f800faef9ebce563e4261739448dda8
Dubomb <dubomb1011@gmail.com>
2024-01-02 10:51:42 -0500

    Created base for frontend that renders different elements based on the server connection status

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from 'react';
+
+async function testConnection() {
+  try {
+    const response = await fetch('http://localhost:3001/status');
+  
+    if (!response.ok) {
+      console.log('Request failed. Request status: ' + response.status);
+      return false;
+    }
+  
+    return true;
+  } catch (err) {
+    console.log('An error has occured: ' + err);
+    return false;
+  }
+}
 
 function App() {
+  const [statusReady, setStatusReady] = useState(false);
+  const [serverSuccess, setServerSuccess] = useState(false);
+
+  testConnection().then(result => {
+    setServerSuccess(result); 
+    setStatusReady(true);
+  });
+
+  if (!statusReady) {
+    return (
+      <div>
+        <p>Loading</p>
+      </div>
+      );
+  }
+
+  if (!serverSuccess) {
+    return (
+    <div>
+      <p>Connection to server failed.</p>
+    </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Success!</p>
     </div>
   );
 }
