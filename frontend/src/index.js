e21f300862dee6886e47babc2ac16f8d5ac0d52c
Dubomb <dubomb1011@gmail.com>
2024-01-02 14:03:52 -0500

    Created app.scss as the main style sheet

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './Styles/index.css';
+import './Styles/app.scss';
 import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
