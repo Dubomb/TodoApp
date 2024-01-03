2d2e20b58fe27b54d80133f1cd9b253b272fab7a
Dubomb <dubomb1011@gmail.com>
2024-01-03 15:07:12 -0500

    Implemented dictionary states for loading task and category data and displaying it

--- frontend/src/Components/Tasks/taskitem.js
+++ frontend/src/Components/Tasks/taskitem.js
@@ -1,9 +1,10 @@
-function TaskItem({t}) {
+function TaskItem({t, c}) {
     return (
         <div>
             <h3>{t.title}</h3>
             <p>{t.description}</p>
             <p>Due: {t.due_date}</p>
+            <p>Category: {c.name}</p>
         </div>
     );
 }
--- frontend/src/Components/Tasks/tasklist.js
+++ frontend/src/Components/Tasks/tasklist.js
@@ -19,21 +19,63 @@ async function getTasks() {
     }
 }
 
+async function getCategories() {
+    try {
+        const response = await fetch('http://localhost:3001/api/categories');
+    
+        if (!response.ok) {
+            return [];
+        }
+    
+        const result = await response.json();
+        return result.message;
+        
+    } catch (err) {
+        console.log('Error when fetching tasks: ' + err.message);
+        return [];
+    }
+}
+
 function TaskList() {
-    const [tasksReady, setTasksReady] = useState(false);
-    const [tasks, setTasks] = useState([]);
+    const [tasksStatus, setTasksStatus] = useState(false);
+    const [categoriesStatus, setCategoriesStatus] = useState(false);
+    const [tasks, setTasks] = useState(new Map());
+    const [categories, setCategories] = useState(new Map());
 
     useEffect(() => {
         getTasks().then(result => {
-            setTasks(result);
-            setTasksReady(true);
+            let m = new Map();
+            for (const item of result) {
+                const {task_ID, ...val} = item;
+                m.set(task_ID, val);
+            }
+            setTasks(m);
+            setTasksStatus(true);
         });
     }, []);
+
+    useEffect(() => {
+        getCategories().then(result => {
+            let m = new Map();
+            for (const item of result) {
+                const {category_ID, ...val} = item;
+                m.set(category_ID, val);
+            }
+            setCategories(m);
+            setCategoriesStatus(true);
+        });
+    }, []);
+
+    if (!tasksStatus || !categoriesStatus) {
+        return (
+            <p>Loading task data...</p>
+        );
+    }
     
     let taskComponents = [];
 
-    for (const task of tasks) {
-        taskComponents.push(<TaskItem t={task}/>)
+    for (const [id, task] of tasks) {
+        taskComponents.push(<TaskItem t={task} c={categories.get(task.category_ID)}/>)
     }
 
     return (
