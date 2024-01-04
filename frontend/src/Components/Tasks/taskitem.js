7c98ba195e6e07427194397823579c481fe418e5
Dubomb <dubomb1011@gmail.com>
2024-01-04 12:22:50 -0500

    Task items now display due date relative to current time

--- frontend/src/Components/Tasks/taskitem.js
+++ frontend/src/Components/Tasks/taskitem.js
@@ -1,15 +1,37 @@
+function calculateDueTime(milliseconds) {
+    const hours = Math.floor(milliseconds / (1000 * 60 * 60));
+    if (Math.abs(hours) < 24) {
+        if (hours === 0) {
+        return 'less than an hour';
+        }
+        return hours + ' hour' + (hours !== 1 ? 's' : '');
+    } else {
+        let days = Math.floor(hours / 24);
+        return days + ' day' + (days !== 1 ? 's' : '');
+    }
+}
+
 function TaskItem({t, c}) {
-    const rectStyle = {
+    const borderStyle = {
         '--accent-color': c.color
     };
 
+    const timeDifference = new Date(t.due_date) - new Date()
+    let due_time;
+    if (timeDifference > 0) {
+        due_time = <p>Due in {calculateDueTime(timeDifference)}</p>;
+    } else {
+        due_time = <p>Was due {calculateDueTime(-timeDifference)} ago</p>;
+    }
+
     return (
-        <div className='task-container' style={rectStyle}>
-            <h3>{t.title}</h3>
+        <div className='task-container' style={borderStyle}>
+            <p><b>{t.title}</b> - {c.name}</p>
+            <p>{due_time}</p>
+            <br></br>
             <p>{t.description}</p>
-            <p>{t.due_date}</p>
-            <p>{t.status_ID}</p>
-            <p>Category: {c.name}</p>
+            <br></br>
+            <p>{t.status_ID ? 'Complete' : 'Incomplete'}</p>
         </div>
     );
 }
